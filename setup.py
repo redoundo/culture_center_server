581a5e52e93fa49ef566327a84b5af01d17f794a
redoundo <vivith0987@gmail.com>
2024-05-14 10:46:04 +0900

    addsetup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='culturecenter',
+    version='0.0.1',
+    packages=find_packages(include=['appserver', 'crawl', "utils", ".github"]),
+    install_requires=[
+         "pip", "APScheduler", "Flask", "PyJWT", "SQLAlchemy", "firebase-admin", "mysql-connector-python"
+    ]
+)
+
+
