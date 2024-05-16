18e314dd44431d59190839a4601ab8a08599da15
redoundo <vivith0987@gmail.com>
2024-05-16 16:09:17 +0900

    scripts

--- scripts/application-stop.ps1
+++ scripts/application-stop.ps1
@@ -1,3 +1,12 @@
 c:\temp\codedeploy-agent.msi /quiet /l c:\temp\host-agent-install-log.txt
 powershell.exe -Command Restart-Service -Name codedeployagent
-docker rm -f crawl_container
+$containerName="crawl_container"
+$containerExists = docker ps -a --format '{{.Names}}' | Where-Object { $_ -eq $containerName }
+if ($containerExists) {
+    # 컨테이너가 존재하면, 컨테이너를 종료합니다.
+    docker stop $containerName
+    Write-Host "컨테이너 $containerName 이 종료되었습니다."
+} else {
+    # 컨테이너가 존재하지 않으면, 메시지를 출력합니다.
+    Write-Host "컨테이너 $containerName 이 존재하지 않습니다."
+}
