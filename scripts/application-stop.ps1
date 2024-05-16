194a27bd18f6b66bebca7e1cbdd2bb26c806bf68
redoundo <vivith0987@gmail.com>
2024-05-16 17:16:09 +0900

    scripts

--- scripts/application-stop.ps1
+++ scripts/application-stop.ps1
@@ -1,5 +1,4 @@
 choco install docker-cli
-
 c:\temp\codedeploy-agent.msi /quiet /l c:\temp\host-agent-install-log.txt
 #powershell.exe -Command Restart-Service -Name codedeployagent
 #$containerName="crawl_container"
