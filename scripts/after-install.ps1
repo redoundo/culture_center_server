6e2de2fc0a58fa4567b3fb1f25f92b080d150b0a
redoundo <vivith0987@gmail.com>
2024-05-21 18:46:35 +0900

    changed

--- scripts/after-install.ps1
+++ scripts/after-install.ps1
@@ -2,3 +2,5 @@ python install pip
 pip install --upgrade pip
 pip install --upgrade setuptools
 pip install APScheduler Flask PyJWT SQLAlchemy firebase-admin mysql-connector-python requests python-dotenv werkzeug
+docker system purne -a
+y
