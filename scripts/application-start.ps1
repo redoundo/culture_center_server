129711d16452215ad975852aa78a1afa945a48e5
redoundo <vivith0987@gmail.com>
2024-05-22 20:01:39 +0900

    changed

--- scripts/application-start.ps1
+++ scripts/application-start.ps1
@@ -1,4 +1,5 @@
 cd /home/app
 docker image build -t crawlers:latest -f Dockerfile .
-docker container run -d --name crawl_container --env-file .env -v crawled:/home/app/crawl crawlers:latest
-python app.py
+docker container run -rm -d --name crawl_container --env-file .env -v crawled:/home/app/crawl crawlers:latest
+docker image build -t flask-server:latest -f Dockerfile-flask .
+docker container run -rm -p 8079:2081 -d --name server_container --env-file .env flask-server:latest
