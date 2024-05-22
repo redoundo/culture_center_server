64befeb68dfcdfeb70bd856ab391736a06c523a0
redoundo <vivith0987@gmail.com>
2024-05-22 20:58:28 +0900

    updatedscripts

--- scripts/application-start.ps1
+++ scripts/application-start.ps1
@@ -1,5 +1,5 @@
 cd /home/app
-docker image build -t crawlers:latest -f Dockerfile .
 docker image build -t flask-server:latest -f Dockerfile-flask .
-docker container run -d --name crawl_container --env-file .env -v crawled:/crawl crawlers:latest
+docker image build -t crawlers:latest -f Dockerfile .
+docker container run -d --name crawl_container --env-file .env -v /crawl crawlers:latest
 docker container run -p 8079:2081 -d --name server_container --env-file .env flask-server:latest
