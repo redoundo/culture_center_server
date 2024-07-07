1533e2651cbae7c14bc3a05863f4763d14a3a6be
redoundo <vivith0987@gmail.com>
2024-07-07 15:58:36 +0900

    playwright 의존성 추가,limit req 대상 변경

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,7 +3,7 @@ echo "application-start executed!!"
 sudo chmod 666 /var/run/docker.sock
 cd /home/app
 docker image build -t crawlers:latest -f Dockerfile .
-docker container run -d --name crawl_container --env-file .env -v /home/app/crawl/sample:/crawl/sample crawlers:latest
+docker container run -d --name crawl_container --restart=always --env-file .env -v /home/app/crawl/sample:/crawl/sample crawlers:latest
 
 docker image build -t java-server:latest -f Dockerfile-java .
-docker container run -p 8079:8079 -d --name server_container --env-file .env -v /home/app/sample:/java-server/sample java-server:latest
+docker container run -p 8079:8079 -d --name server_container --restart=always --env-file .env -v /home/app/sample:/java-server/sample java-server:latest
