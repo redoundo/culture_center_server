de6e52b1de55df8cee91696090a57a97d674357a
redoundo <vivith0987@gmail.com>
2024-07-11 17:14:24 +0900

    start period 시간 변경

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -5,4 +5,4 @@ docker image build -t crawlers:latest -f Dockerfile .
 docker container run -d --name crawl_container --env-file .env -v /home/app/crawl/sample:/crawl/sample crawlers:latest
 
 docker image build -t java-server:latest -f Dockerfile-java .
-docker container run -p 8079:8079 -d --name server_container --restart always --env-file .env -v /home/app/sample:/java-server/sample java-server:latest
+docker container run -p 8079:8079 -d --name server_container --restart always --env-file .env -v /var/log/spring_boot:/var/log/spring-boot -v /var/log/nginx:/var/log/nginx -v /home/app/sample:/java-server/sample java-server:latest
