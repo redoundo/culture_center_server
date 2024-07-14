b4ec6aff19884def4746eb164885fa89967a1241
redoundo <vivith0987@gmail.com>
2024-07-14 14:28:03 +0900

    limit rate 조절

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -6,7 +6,7 @@ docker container run -d --name crawl_container --env-file .env -v /home/app/craw
 
 docker image build -t java-server:latest -f Dockerfile-java .
 docker container run -p 8079:8079 -d --name server_container --restart always --env-file .env \
---health-cmd='curl -f http://localhost:8079/health/check || kill 1' --health-interval=1m --health-retries=5 --health-timeout=20s \
+--health-cmd='curl -f http://localhost:8079/health/check || kill 1' --health-interval=30s --health-retries=5 --health-timeout=20s \
 --health-start-period=3m30s --health-start-interval=1m \
  -v /var/log/spring_boot:/var/log/spring-boot -v /var/log/nginx:/var/log/nginx \
  -v /home/app/sample:/java-server/sample java-server:latest
